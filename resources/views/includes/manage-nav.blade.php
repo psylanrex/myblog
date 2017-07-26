785b61e0fe1d335bf1a153f53bc04b20c9be898e
Kha Thantron <kthantron@Khas-MacBook-Pro.local>
2017-07-26 13:44:39 -0700

    Fixed logout

--- resources/views/includes/manage-nav.blade.php
+++ resources/views/includes/manage-nav.blade.php
@@ -1,5 +1,10 @@
 <div class="side-menu">
     <aside class="menu m-t-30 m-l-10">
+        @if(Auth::check())
+            <p class="menu-label is-aligned-center">
+                Hi {{ Auth::user()->name }}!
+            </p>
+        @endif
         <p class="menu-label">
             General
         </p>
@@ -17,6 +22,16 @@
             </li>
             <li>
                 <a href="">Roles &amp; Permissions</a>
+            </li>
+        </ul>
+            <hr class="dropdown-divider">
+        <ul class="menu-list">
+            <li>
+                <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit()">
+                    Logout
+                    <span class="icon"><i class="fa fa-fw fa-sign-out"></i></span>
+                </a>
+                @include('includes.forms.logout')
             </li>
         </ul>
     </aside>
